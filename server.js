1fc4c5053631d897baa42d72e7b75939557dff5d
Sathyanarayanan-R <rsathyajana2000@gmail.com>
2023-11-10 06:40:54 +0530

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({origin: "https://transaction-dashboard-backend-sj.onrender.com"}));
 
 app.use("/seeddata", seedDataRoute);
 app.use("/products", productRoutes);
