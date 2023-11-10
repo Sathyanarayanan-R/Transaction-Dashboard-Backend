c1caa0e3183644a6b50667130d0dc4d8789bcf2a
Sathyanarayanan-R <rsathyajana2000@gmail.com>
2023-11-10 06:32:05 +0530

    update

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getProductsBySearchAndPagination, getMonthlySalesStatistics, getMonthlyPriceRange, getMonthlyCategories, getAll3APIData } from '../controllers/products.js';
+import { getProductsBySearchAndPagination, getMonthlySalesStatistics, getMonthlyPriceRange, getMonthlyCategories, getAllAPIData } from '../controllers/products.js';
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.get('/search/:month', getProductsBySearchAndPagination);
 router.get('/monthlysale/:month', getMonthlySalesStatistics);
 router.get('/monthlypricerange/:month', getMonthlyPriceRange);
 router.get('/monthlycategories/:month', getMonthlyCategories);
-router.get('/getAll3APIData/:month', getAll3APIData);
+router.get('/getallapidata/:month', getAllAPIData);
 
 export default router;
